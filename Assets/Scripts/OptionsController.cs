d5bcb6c255b57da2618a14e9f6de1a1efe0287a3
Andrew L <alavaia@yahoo.com>
2018-11-14 23:56:56 -0500

    Added simple framework for adding radio buttons to options menu

--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -9,6 +9,12 @@ public class OptionsController : MonoBehaviour {
 	public Sprite switchOn;
 	public Sprite switchOff;
 
+	public Button delayButtonShort;
+	public Button delayButtonMedium;
+	public Button delayButtonLong;
+	public Sprite radioChecked;
+	public Sprite radioUnchecked;
+
 	// Use this for initialization
 	void Start () {
 		if (PlayerPrefs.GetInt ("SETTINGS_Sound") == 0) {
@@ -16,24 +22,55 @@ public class OptionsController : MonoBehaviour {
 		} else {
 			optionButton.GetComponent<Image> ().sprite = switchOn;
 		}
+
+		if (PlayerPrefs.GetInt ("SETTINGS_Delay") == 0) {
+			delayButtonShort.GetComponent<Image> ().sprite = radioChecked;
+			delayButtonMedium.GetComponent<Image> ().sprite = radioUnchecked;
+			delayButtonLong.GetComponent<Image> ().sprite = radioUnchecked;		
+		} else if (PlayerPrefs.GetInt ("SETTINGS_Delay") == 2) {
+			delayButtonShort.GetComponent<Image> ().sprite = radioUnchecked;
+			delayButtonMedium.GetComponent<Image> ().sprite = radioUnchecked;
+			delayButtonLong.GetComponent<Image> ().sprite = radioChecked;
+		} else { // default to 1
+			delayButtonShort.GetComponent<Image> ().sprite = radioUnchecked;
+			delayButtonMedium.GetComponent<Image> ().sprite = radioChecked;
+			delayButtonLong.GetComponent<Image> ().sprite = radioUnchecked;	
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	public void pressSwitch() {
-		
 		if (optionButton.GetComponent<Image> ().sprite.Equals(switchOn) ) {
 			optionButton.GetComponent<Image> ().sprite = switchOff;
 			PlayerPrefs.SetInt ("SETTINGS_Sound", 0);
 		} else {
 			optionButton.GetComponent<Image> ().sprite = switchOn;
 			PlayerPrefs.SetInt ("SETTINGS_Sound", 1);
-	
 		}
-
-
 	}
+
+	public void pressDelayButtonShort() {
+		delayButtonShort.GetComponent<Image> ().sprite = radioChecked;
+		delayButtonMedium.GetComponent<Image> ().sprite = radioUnchecked;
+		delayButtonLong.GetComponent<Image> ().sprite = radioUnchecked;	
+		PlayerPrefs.SetInt ("SETTINGS_Delay", 0);
+	}
+
+	public void pressDelayButtonMedium() {
+		delayButtonShort.GetComponent<Image> ().sprite = radioUnchecked;
+		delayButtonMedium.GetComponent<Image> ().sprite = radioChecked;
+		delayButtonLong.GetComponent<Image> ().sprite = radioUnchecked;	
+		PlayerPrefs.SetInt ("SETTINGS_Delay", 1);
+	}
+
+	public void pressDelayButtonLong() {
+		delayButtonShort.GetComponent<Image> ().sprite = radioUnchecked;
+		delayButtonMedium.GetComponent<Image> ().sprite = radioUnchecked;
+		delayButtonLong.GetComponent<Image> ().sprite = radioChecked;	
+		PlayerPrefs.SetInt ("SETTINGS_Delay", 2);
+	}
+
 }
